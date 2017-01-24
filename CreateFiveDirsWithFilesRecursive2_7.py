cbef6d386b19faaf080be265dfc0b132222eeb90
Pragad <talk2pragadheesh@gmail.com>
2017-01-23 17:26:51 -0800

    Creating Repeated Files and Folders

---
+++ CreateFiveDirsWithFilesRecursive2_7.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env/python
+import os
+
+baseFileName = raw_input("Enter base folder name: ")
+
+myPwd = os.getcwd()
+
+for index in range(0, 5):
+    if not os.path.exists(baseFileName + str(index)):
+        os.makedirs(baseFileName + str(index))
+
+        os.chdir(myPwd + '/' + baseFileName + str(index))
+        myPwd = os.getcwd()
+
+        for fileNameIdx in range(0, 10):
+            fileOpen = open(baseFileName + str(index) + "_" + str(fileNameIdx), "w+")
+            fileOpen.write("abcdefghijklmnopqrstuvwxy")
+            fileOpen.close
