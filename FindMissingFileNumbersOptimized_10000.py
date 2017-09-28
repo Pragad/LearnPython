171edac5ba4ef609a5acdd216e8206c9995c68a0
Pragad <talk2pragadheesh@gmail.com>
2017-09-28 16:29:56 -0700

    Fixing findFiles script

--- FindMissingFileNumbersOptimized_10000.py
+++ FindMissingFileNumbersOptimized_10000.py
@@ -8,11 +8,11 @@ import re
 def main():
     filePath = input("Enter file path: ")
     endFileNum = input("Enter end file number: ")
-    filesMissing = []
-    filesPresent = []
     filesMissingCount = 0
     filesPresentCount = 0
-    present_ones = set()
+    fileNumbersMissing = set()
+    filesNumbersPresent = set()
+    fileNamesPresent = set()
 
     #>>> myRegex=r'(.*)file_(.)' + re.escape(str(TEXTTO)) + r'\.txt'
     #>>> searchObj=re.search(myRegex,line)
@@ -22,22 +22,25 @@ def main():
     # https://stackoverflow.com/questions/46476537/python-how-to-search-a-string-in-a-large-file
     my_regex = re.compile('.*file_.(\d+)\.txt.*')
     for line in open(filePath):
-        match = my_regex.match(line)
+        match = my_regex.search(line)
         if match:
-            present_ones.add(int(match.group(1)))
+            filesNumbersPresent.add(int(match.group(1)))
+            start = line.find('file_')
+            end = line.find('txt', start)
+            fileNamesPresent.add(line[start:end+3])
 
     for x in range(int(endFileNum)):
-        if x not in present_ones:
-            filesMissing.append(x)
+        if x not in filesNumbersPresent:
+            fileNumbersMissing.add(x)
             filesMissingCount += 1
         else:
-            filesPresent.append(x)
             filesPresentCount += 1
-    #print("Files Present: ")
-    #print(filesPresent)
+    print("Files Present: ")
+    print(fileNamesPresent)
+    #print(filesNumbersPresent)
     print("Files Present Count: %d" % filesPresentCount)
     print("Files Missing: ")
-    print(filesMissing)
+    print(fileNumbersMissing)
     print("Files Missing Count: %d" % filesMissingCount)
 
 #-------------------------------------------------------------------------------------------
